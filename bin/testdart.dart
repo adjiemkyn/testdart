cc3a15826cecd33218f8b93ce46ed9a30694123c
adjiemkyn <adjiemikayana@gmail.com>
2024-03-12 21:49:45 +0800

    pewarisan

--- bin/testdart.dart
+++ bin/testdart.dart
@@ -1,37 +1,24 @@
 import 'package:testdart/testdart.dart' as testdart;
 
-class Mobil {
+class Kendaraan {
   String merk;
-  int tahunProduksi;
-// Konstruktor dengan null safety
-  Mobil(this.merk, this.tahunProduksi);
-}
-
-class Akun {
-  String? namaPengguna; // Bisa bernilai null
-  String email; // Tidak boleh bernilai nul
-  Akun(this.email, {this.namaPengguna});
-}
-
-class Kalkulator {
-  int tambah(int a, int b) {
-    return a + b;
+  Kendaraan(this.merk);
+  void klakson() {
+    print("Tin! Tin!");
   }
+}
 
-  int? bagi(int a, int b) {
-    if (b == 0) return null; // Dapat mengembalikan null
-    return a ~/ b;
+class Mobil extends Kendaraan {
+  int tahunProduksi;
+  Mobil(String merk, this.tahunProduksi) : super(merk);
+  @override
+  void klakson() {
+    print("Tut! Tut!");
   }
 }
 
 void main() {
-  var mobilSaya = Mobil('Toyota', 2021);
-  print('Merk Mobil: ${mobilSaya.merk}, Tahun: ${mobilSaya.tahunProduksi}');
-  var akunSaya = Akun('example@example.com');
-  print(
-      'Email: ${akunSaya.email}, Nama Pengguna: ${akunSaya.namaPengguna ?? 'tidak diketahui'}');
-  var kalkulator = Kalkulator();
-  print('Hasil tambah: ${kalkulator.tambah(10, 5)}');
-  var hasilBagi = kalkulator.bagi(10, 0);
-  print('Hasil bagi: ${hasilBagi ?? 'Operasi tidak valid'}');
+  var mobilSaya = Mobil("Toyota", 2020);
+  mobilSaya.klakson(); // Output: Tut! Tut!
+  print(mobilSaya.merk); // Output: Toyota
 }
