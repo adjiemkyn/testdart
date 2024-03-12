8beeef1c433434c378150ef256ee61b649f34964
adjiemkyn <adjiemikayana@gmail.com>
2024-03-12 21:13:35 +0800

    fungsi

--- bin/testdart.dart
+++ bin/testdart.dart
@@ -1,10 +1,10 @@
 import 'package:testdart/testdart.dart' as testdart;
 
 void main() {
-  for (int i = 0; i < 10; i++) {
-    if (i % 2 == 0) {
-      continue; // Lewati iterasi saat ini jika i adalah bilangan genap
-    }
-    print('Bilangan ganjil: $i');
-  }
+  namaFungsi(); // fungsi dipanggil
+}
+
+void namaFungsi() {
+  // deklarasi fungsi
+  print("halo fungsi!");
 }
