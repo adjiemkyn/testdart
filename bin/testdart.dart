904fbb28a3b5f6e9aaf5d5822c012572883b7c72
adjiemkyn <adjiemikayana@gmail.com>
2024-03-12 21:17:53 +0800

    parameter posisional

--- bin/testdart.dart
+++ bin/testdart.dart
@@ -1,10 +1,10 @@
 import 'package:testdart/testdart.dart' as testdart;
 
 void main() {
-  namaFungsi(); // fungsi dipanggil
+  sapa('Alice'); // Output: Halo, Alice!
+  sapa('Bob', 'Selamat datang'); // Output: Selamat datang, Bob!
 }
 
-void namaFungsi() {
-  // deklarasi fungsi
-  print("halo fungsi!");
+void sapa(String nama, [String salam = 'Halo']) {
+  print('$salam, $nama!');
 }
