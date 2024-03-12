0bf593b65712536b65d1aab50a8ed4ea74b50158
adjiemkyn <adjiemikayana@gmail.com>
2024-03-12 20:28:08 +0800

    operator tipe

--- bin/testdart.dart
+++ bin/testdart.dart
@@ -1,12 +1,7 @@
 import 'package:testdart/testdart.dart' as testdart;
 
 void main() {
-  int a = 10;
-  int b = 5;
-  bool isEqual = a == b; // false
-  bool isNotEqual = a != b; // true
-  bool isGreaterThan = a > b; // true
-  bool isLessThan = a < b; // false
-  bool isGreaterOrEqual = a >= b; // true
-  bool isLessOrEqual = a <= b; // false
+  var number = 100;
+  bool isInteger = number is int; // true
+  bool isNotInteger = number is! int;
 }
