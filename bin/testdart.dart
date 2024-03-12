edb12b7124593f4f229f4baea1a9087019b8d7fd
adjiemkyn <adjiemikayana@gmail.com>
2024-03-12 21:24:02 +0800

    fungsi tanpa pengembalian nilai

--- bin/testdart.dart
+++ bin/testdart.dart
@@ -1,22 +1,7 @@
 import 'package:testdart/testdart.dart' as testdart;
 
 void main() {
-// Fungsi yang mengembalikan hasil penjumlahan dua bilangan.
-  int tambah(int a, int b) {
-    return a + b;
+  void sapaPengguna(String nama) {
+    print('Halo, $nama!');
   }
-
-// Fungsi yang mengembalikan teks berdasarkan kondisi.
-  String kategorikanUmur(int umur) {
-    if (umur < 18) {
-      return 'Remaja';
-    } else {
-      return 'Dewasa';
-    }
-  }
-}
-
-// Fungsi yang mengembalikan nilai boolean.
-bool apakahGenap(int angka) {
-  return angka % 2 == 0;
 }
