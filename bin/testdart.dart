30da7d00aa710b6ef78a58a5332ed222df7fee4c
adjiemkyn <adjiemikayana@gmail.com>
2024-03-12 20:32:29 +0800

    operator tipe

--- bin/testdart.dart
+++ bin/testdart.dart
@@ -3,5 +3,6 @@ import 'package:testdart/testdart.dart' as testdart;
 void main() {
   var number = 100;
   bool isInteger = number is int; // true
-  bool isNotInteger = number is! int;
+  bool isNotInteger = number is! int; // false
+  (dynamic number as int).isEven;//Menggunakan 'as' untuk meng-cast dynamic ke int.
 }
